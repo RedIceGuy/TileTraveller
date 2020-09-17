a8f0473ae86fe6213572872e2f7cdef66b22f83f
RedIceGuy <eythor0156@gmail.com>
2020-09-17 10:06:34 +0000

    algorithm +

--- tile_traveller.py
+++ tile_traveller.py
@@ -2,8 +2,8 @@
 # https://github.com/RedIceGuy/TileTraveller
 
 # Algorithm
-# 1. 
-# 2. 
-# 3.
-# 4.
-# 5.
+# 1. Skapa reitina (1,1; 1,2...)
+# 2. Slá inn átt
+# 3. Gá hvort það sé veggur
+# 4. Halda áfram ef enginn veggur
+# 5. Reyna aftur ef það er veggur
