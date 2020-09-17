3971749e206d543b70b1d9fe5cd86da2b0c0375d
RedIceGuy <eythor0156@gmail.com>
2020-09-17 10:12:04 +0000

    functions created

--- tile_traveller.py
+++ tile_traveller.py
@@ -2,8 +2,25 @@
 # https://github.com/RedIceGuy/TileTraveller
 
 # Algorithm
-# 1. Skapa reitina (1,1; 1,2...)
+# 1. Skapa reitina (1,1; 1,2...) 
 # 2. Slá inn átt
 # 3. Gá hvort það sé veggur
 # 4. Halda áfram ef enginn veggur
 # 5. Reyna aftur ef það er veggur
+
+def Reitur_1_2():
+
+def Reitur_1_3():
+
+def Reitur_2_1():
+
+def Reitur_2_2():
+
+def Reitur_2_3():
+
+def Reitur_3_1():
+
+def Reitur_3_2():
+
+def Reitur_3_3():
+
