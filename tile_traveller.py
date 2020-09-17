f771ddbf8c8e67a98a66162265789f1817618eb1
RedIceGuy <eythor0156@gmail.com>
2020-09-17 10:24:25 +0000

    byrja forrit

--- tile_traveller.py
+++ tile_traveller.py
@@ -8,19 +8,34 @@
 # 4. Halda áfram ef enginn veggur
 # 5. Reyna aftur ef það er veggur
 
-def Reitur_1_2():
+def Reitur_1_1(direction):
+    print("You can travel: (N)orth.")
+    if direction == "n" or direction == "N":
+        return True
+    else:
+        print("Not a valid direction!")
+        return False
 
-def Reitur_1_3():
+def Reitur_1_2(direction):
 
-def Reitur_2_1():
+def Reitur_1_3(direction):
 
-def Reitur_2_2():
+def Reitur_2_1(direction):
 
-def Reitur_2_3():
+def Reitur_2_2(direction):
 
-def Reitur_3_1():
+def Reitur_2_3(direction):
 
-def Reitur_3_2():
+def Reitur_3_1(direction):
+    # Victory
+    print("Victory!")
 
-def Reitur_3_3():
+def Reitur_3_2(direction):
+    if direction == "n" or direction == "N":
 
+
+def Reitur_3_3(direction):
+
+
+while banana != True:
+    if Reitur_1_1():
