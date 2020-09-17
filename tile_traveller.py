852e3ad9e70b355d82ce53a1b77544df917a406b
RedIceGuy <eythor0156@gmail.com>
2020-09-17 10:39:05 +0000

    Basic movement

--- tile_traveller.py
+++ tile_traveller.py
@@ -8,34 +8,57 @@
 # 4. Halda áfram ef enginn veggur
 # 5. Reyna aftur ef það er veggur
 
-def Reitur_1_1(direction):
+def direction():
+    direction = input("Direction: ")
+    return direction
+
+def Reitur_1_1(dir_str):
     print("You can travel: (N)orth.")
+    direction = dir_str
     if direction == "n" or direction == "N":
         return True
     else:
         print("Not a valid direction!")
         return False
+"""
+def Reitur_1_2():
 
-def Reitur_1_2(direction):
+    direction = direction()
 
-def Reitur_1_3(direction):
+def Reitur_1_3():
 
-def Reitur_2_1(direction):
+    direction = direction()
 
-def Reitur_2_2(direction):
+def Reitur_2_1():
 
-def Reitur_2_3(direction):
+    direction = direction()
 
-def Reitur_3_1(direction):
+def Reitur_2_2():
+
+    direction = direction()
+
+def Reitur_2_3():
+
+    direction = direction()
+
+def Reitur_3_1():
     # Victory
     print("Victory!")
 
 def Reitur_3_2(direction):
+
+    direction = direction()
     if direction == "n" or direction == "N":
 
 
 def Reitur_3_3(direction):
 
+    direction = direction()
+"""
+def move(func):
+    func
+    while not func:
+        func
 
-while banana != True:
-    if Reitur_1_1():
+
+move(Reitur_1_1(direction()))
